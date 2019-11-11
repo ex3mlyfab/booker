671162810294ebd179ac277001ea626ff5334a46
Abdulkadir Olatunji <tunjikadri@gmail.com>
2019-11-11 02:17:26 +0100

    added transaction model controller

---
+++ app/Http/Controllers/TranxController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tranx;
+use Illuminate\Http\Request;
+
+class TranxController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tranx  $tranx
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tranx $tranx)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tranx  $tranx
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tranx $tranx)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tranx  $tranx
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tranx $tranx)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tranx  $tranx
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tranx $tranx)
+    {
+        //
+    }
+}
---
+++ database/factories/TranxFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Tranx;
+use Faker\Generator as Faker;
+
+$factory->define(Tranx::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
